f048f6dec5249a6c732b674f1aa6ebd7b0c319c0
Gabor Jenei <jeneigabor14@gmail.com>
2022-01-26 19:01:39 +0000

    add bash script with variables

---
+++ WelcomeScript.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# This is my second bash script
+# I'll prompt the user for input, allocate the input value to a variable
+# and display it back to the screen
+
+echo "What is your name?"
+read name
+
+echo "What chapter are you on in the book?"
+read chapter
+
+echo "Welcome $name to Chapter $chapter of Linux Basics for Hackers!"
